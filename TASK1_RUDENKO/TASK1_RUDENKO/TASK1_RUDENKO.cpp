efd6d3c2974c5d5b6d8dc51526fb4e12a38957ab
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-26 20:26:03 +0300

    finish task 1

--- TASK1_RUDENKO/TASK1_RUDENKO/TASK1_RUDENKO.cpp
+++ TASK1_RUDENKO/TASK1_RUDENKO/TASK1_RUDENKO.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+void output1();
+void output2();
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    output1();
+    output1();
+    output2();
+    output2();
+}
+
+void output1()
+{
+    std::cout << "Three blind mice\n";
+}
+
+void output2()
+{
+    std::cout << "See how they run\n";
 }
