bad869e7474cb9516fb2988b3f575974ea799749
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-26 21:48:36 +0300

    finish task 2

--- TASK2_RUDENKO/TASK1_RUDENKO/TASK1_RUDENKO.cpp
+++ TASK2_RUDENKO/TASK1_RUDENKO/TASK1_RUDENKO.cpp
@@ -1,20 +1,19 @@
-// TASK1_RUDENKO.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+int convertYearsToMonths(int years)
+{
+	return years * 12;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int age = 0;
+
+	std::cout << "Enter you age: ";
+	std::cin >> age;
+
+	if (age >= 0)
+		std::cout << "Your age in months: " << convertYearsToMonths(age) << ".";
+	else
+		std::cout << "Please, enter a valid age.";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
