7e256e2a970aa0ebcf4d1b115f9b744a3397cad5
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-18 21:27:38 +0300

    the fourth task

--- lab5/4/Lab5/Lab4/Lab4.cpp
+++ lab5/4/Lab5/Lab4/Lab4.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
+#include <string>
 
-using namespace ::std;
+using namespace std;
 
-int main()
-{
-	int a = 0, b = 0;
+int main() {
+    string months[]{
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+    };
+    int sales[12]{ 0 };
 
-start:
-	cout << "Enter an a: " << endl;
-	cin >> a;
+    for (int i = 0; i < 12; i++) {
+        cout << "Enter the number of books sold in " + months[i] + ": ";
+        cin >> sales[i];
+    }
 
-	cout << "Enter a b: " << endl;
-	cin >> b;
+    int totalSales = 0;
 
+    for (int i = 0; i < 12; i++) {
+        totalSales += sales[i];
+    }
 
-	if (a > b) {
-		cout << "Please, enter a valid number..." << endl;
-		goto start;
-	}
-	else if (a == b) {
-		cout << a << endl;
-	}
-	else {
-		cout <<  "\n\nResult: " << ((b - a + 1) * (a + b)) / 2 << endl << endl;
-	}
+    cout << "Total sales for the year: " << totalSales << endl;
+
+    return 0;
 }
