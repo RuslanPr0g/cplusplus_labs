96383c986c83d42e03c5724a7acfc26110abba21
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-18 22:02:18 +0300

    finish

--- lab5/6/Lab5/Lab4/Lab4.cpp
+++ lab5/6/Lab5/Lab4/Lab4.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 
-using namespace ::std;
+int main(int argc, char** argv) {
+    using namespace std;
 
-int main()
-{
-	int a = 0, b = 0;
+    int numRows = 0;
 
-start:
-	cout << "Enter an a: " << endl;
-	cin >> a;
+    cout << "Enter number of rows: ";
+    cin >> numRows;
 
-	cout << "Enter a b: " << endl;
-	cin >> b;
+    int numAsterisks = 1;
+    int numPeriods = numRows - numAsterisks;
 
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numPeriods; j++) {
+            cout << '.';
+        }
 
-	if (a > b) {
-		cout << "Please, enter a valid number..." << endl;
-		goto start;
-	}
-	else if (a == b) {
-		cout << a << endl;
-	}
-	else {
-		cout <<  "\n\nResult: " << ((b - a + 1) * (a + b)) / 2 << endl << endl;
-	}
+        for (int k = 0; k < numAsterisks; k++) {
+            cout << '*';
+        }
+
+        cout << endl;
+
+        numPeriods--;
+        numAsterisks++;
+    }
+
+    return 0;
 }
