dd20f37690bc0a75483157eda0f5cd48bbfa1013
RuslanPr0g <theruslan.prog@gmail.com>
2021-03-31 11:15:14 +0300

    lab6:task3

--- lab6/3/Lab5/Lab4/Lab4.cpp
+++ lab6/3/Lab5/Lab4/Lab4.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
+const int StrSize = 25;
 
-using namespace ::std;
+struct bop
+{
+    char fullName[StrSize];
+    char title[StrSize];
+    char bopName[StrSize];
+    int preference;
+};
 
 int main()
 {
-	int a = 0, b = 0;
 
-start:
-	cout << "Enter an a: " << endl;
-	cin >> a;
+    bop member[4] = { "Scott Doe", "Hacker", "Amarok", 2,
+                      "Jessica Doe", "Gamer", "Laelith", 0,
+                      "Sammy Farha", "Dog", "Samson", 1,
+                      "Albert Einstein", "Genius", "Relativity", 2 };
 
-	cout << "Enter a b: " << endl;
-	cin >> b;
+    std::cout << "Benevolent Order of Programmers Report\n";
+    std::cout << "a) display name       b) display title\n";
+    std::cout << "c) display bopname    d) display preference\n";
+    std::cout << "q) quit\n";
 
+    char choice;
+    do
+    {
+        std::cout << ">>> ";
+        std::cin >> choice;
+        choice = tolower(choice);
 
-	if (a > b) {
-		cout << "Please, enter a valid number..." << endl;
-		goto start;
-	}
-	else if (a == b) {
-		cout << a << endl;
-	}
-	else {
-		cout <<  "\n\nResult: " << ((b - a + 1) * (a + b)) / 2 << endl << endl;
-	}
+        for (int i = 0; i < 4; i++)
+        {
+            switch (choice)
+            {
+            case 'a': std::cout << member[i].fullName << std::endl; break;
+            case 'b': std::cout << member[i].title << std::endl; break;
+            case 'c': std::cout << member[i].bopName << std::endl; break;
+            case 'd': switch (member[i].preference)
+            {
+            case 0: std::cout << member[i].fullName << std::endl; break;
+            case 1: std::cout << member[i].title << std::endl; break;
+            case 2: std::cout << member[i].bopName << std::endl; break;
+            };
+            }
+        }
+    } while (choice != 'q');
+
+    std::cout << "\nBye!\n";
+    return 0;
 }
